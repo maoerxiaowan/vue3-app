a4348d72527627983267db9722733c17c15c1c0d
nekowan <1076875959@qq.com>
2023-08-12 21:21:27 +0800

    初步提交

---
+++ src/views/login/login.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="background" id="app">
+        <form action="">
+            <div class="foreground">
+                <h3>教学成果档案袋管理系统</h3>
+                <div class="from-input">
+                    <input type="text" class="input" placeholder="  用户名" />
+                    <input type="password" class="input" placeholder="  密  码" />
+                </div>
+                <div class="from-button">
+                    <input type="button" value="登录" />
+                    <input type="button" value="注册" />
+                </div>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+
+</script>
